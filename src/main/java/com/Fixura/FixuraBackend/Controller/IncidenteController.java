0bfaa18269f750d594688ef7d4455c1d32caa7ad
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-09-25 21:28:15 -0500

    Configuración de ruteo para el uso de CORS

--- src/main/java/com/Fixura/FixuraBackend/Controller/IncidenteController.java
+++ src/main/java/com/Fixura/FixuraBackend/Controller/IncidenteController.java
@@ -25,7 +25,7 @@ import java.text.ParseException;
 
 @RestController
 @RequestMapping("/api/incidente")
-@CrossOrigin("*")
+@CrossOrigin(origins = "http://localhost:4200")
 public class IncidenteController {
 
     @Autowired
