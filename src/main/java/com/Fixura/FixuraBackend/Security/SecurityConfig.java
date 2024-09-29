6e91b5b305d4baae31e269cc032cc6dc092118a9
Pablo AQ <alfaroquispepablo@gmail.com>
2024-09-28 21:42:14 -0500

    modificación: Se elimino variables no usadas de SecurityConfig.java

--- src/main/java/com/Fixura/FixuraBackend/Security/SecurityConfig.java
+++ src/main/java/com/Fixura/FixuraBackend/Security/SecurityConfig.java
@@ -12,18 +12,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+
 @Configuration
 public class SecurityConfig {
 
     @Autowired
     private JwtRequestFilter jwtRequestFilter;
 
-    @SuppressWarnings("removal")
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-            .cors()
-            .and()
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
 
