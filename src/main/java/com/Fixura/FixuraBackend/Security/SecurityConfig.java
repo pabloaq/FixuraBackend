f2e48f432564f9ec2ab0c0e57dac889fa722ff32
JordanRondon <jordanangelrondonpozo@gmail.com>
2024-10-31 22:33:43 -0500

    Agregación: endpoint para listar incidencias consolidadas

--- src/main/java/com/Fixura/FixuraBackend/Security/SecurityConfig.java
+++ src/main/java/com/Fixura/FixuraBackend/Security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/incidente/list/paginated/usuario").permitAll()
                         .requestMatchers("/api/incidente/list/paginated/usuario_distrito").permitAll()
                         .requestMatchers("/api/incidente/list/paginated/distrito").permitAll()
+                        .requestMatchers("/api/incidente/list/paginated/consolidado").permitAll()
 
                         .requestMatchers("/api/v1/departamento/**").permitAll()
                         .requestMatchers("/api/incidente/list/usuario/**").permitAll()
